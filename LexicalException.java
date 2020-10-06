c3f1031479a05600d61e8871ffa16c84355a6a2b
Constant Pagoui <constantpagoui@gmail.com>
2020-10-06 11:44:23 -0400

    Add files via upload
    This portion of the project involved writing several programs to work toward the overall objective of
    designing the elements of a programming language. The basics of software development from design to
    implementation were employed by the project partners.

---
+++ LexicalException.java
@@ -0,0 +1,9 @@
+
+public class LexicalException extends Exception{
+
+	public LexicalException(String errorMessage) {
+		// TODO Auto-generated constructor stub
+		super(errorMessage);
+	}
+
+}
---
+++ TokenType.java
@@ -0,0 +1,33 @@
+/**
+ * @author lpagoui
+ * Defining different token types in our grammar
+ */
+public enum TokenType {
+	
+	    PROGRAM_TKN,
+	    for_statement,
+	    identifier,
+	    integer,
+	    keyword,
+	    string_literal,
+	    add_operator,
+	    sub_operator,
+	    multiply_operator,
+	    division_operator,
+	    assignment_operator,
+	    set_statement,
+		left_parenthesis,
+		right_parenthesis,
+		le_operator,
+		lt_operator,
+		ge_operator,
+		gt_operator,
+    	eq_operator, 
+		comments_bracket,
+		arithmetic_expression,
+		concat_operator,
+		while_statement,
+		exit_statement,
+		EOS_TOK,
+	
+}
